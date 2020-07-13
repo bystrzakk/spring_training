3a0a351fd4bf4718c09e0816d247be31bc06af5b
Mateusz Bystrzycki <mateusz.bystrzycki@bisnode.com>
2020-07-13 12:51:14 +0200

    Init spring core project

---
+++ src/main/java/test/demo/spring/core/person/BackPack.java
@@ -0,0 +1,14 @@
+package test.demo.spring.core.person;
+
+public class BackPack {
+
+    private int capacity;
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+}

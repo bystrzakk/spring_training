3a8c60b86369208e2bc087888018d4c652f5e225
Mateusz Bystrzycki <mateusz.bystrzycki@bisnode.com>
2020-07-15 14:17:30 +0200

    spring rest tomcat example - no spring boot

--- src/main/java/test/demo/spring/core/Application.java
+++ src/main/java/test/demo/spring/core/Application.java
@@ -1,13 +1,21 @@
 package test.demo.spring.core;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SpringBootApplication
-public class Application {
+import java.util.List;
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+@EnableWebMvc
+@Configuration
+@ComponentScan("test.demo.spring.core")
+public class Application implements WebMvcConfigurer {
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(new MappingJackson2HttpMessageConverter());
     }
-
 }

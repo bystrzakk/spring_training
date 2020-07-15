88ee20cfdd8c0fbb3dd24e05d24dbfbf8eae1b3a
Mateusz Bystrzycki <mateusz.bystrzycki@bisnode.com>
2020-07-15 14:04:37 +0200

    init spring boot

--- src/main/java/test/demo/spring/core/Application.java
+++ src/main/java/test/demo/spring/core/Application.java
@@ -1,10 +1,13 @@
 package test.demo.spring.core;
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Configuration
-@ComponentScan
+@SpringBootApplication
 public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
 }
